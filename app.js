21ca7a74a41e1265da20300a536b0fa338871e6b
yotaadachi <yota961219@gmail.com>
2021-11-20 15:47:29 +0900

    boolean型の学習

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('入力値が正しくありません');
+function add(n1, n2, showResult, phrase) {
+    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error('入力値が正しくありません')
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result: ';
+add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
-function add(n1: number ,n2: number) {
-  if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    throw new Error('入力値が正しくありません')
+function add(n1: number ,n2: number, showResult: boolean, phrase: string) {
+  // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('入力値が正しくありません')
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
   }
-  return n1 + n2;
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result: ';
 
-const result = add(number1, number2);
-console.log(result);
+ add(number1, number2, printResult, resultPhrase);
