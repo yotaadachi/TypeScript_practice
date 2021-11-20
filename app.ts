b83c6875b075b26282ce46d8510607f715200c90
yotaadachi <yota961219@gmail.com>
2021-11-20 16:34:16 +0900

    object型の学習

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-function add(n1: number ,n2: number, showResult: boolean, phrase: string) {
-  // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('入力値が正しくありません')
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'yota',
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result: ';
-
- add(number1, number2, printResult, resultPhrase);
+console.log(person);
