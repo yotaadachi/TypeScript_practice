ab5f3fda3fe25602e65b6f196fec04a902beb0cc
yotaadachi <yota961219@gmail.com>
2021-11-20 21:44:30 +0900

    union型の学習

---
+++ objs-arrays-enum.ts
@@ -0,0 +1,42 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN, 
+  READ_ONLY, 
+  AUTHOR,
+}
+
+const person = {
+  name: 'yota',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('管理者ユーザ');
+}
