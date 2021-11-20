9d68dce8c14d377aeb0ca9445ab069f534329806
yotaadachi <yota961219@gmail.com>
2021-11-20 21:21:24 +0900

    Enum型の学習

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'yota',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 // person.role.push('admin');
 // person.role[1] = 10;
@@ -13,4 +30,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('管理者ユーザ');
 }
--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'yota',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN, 
+  READ_ONLY, 
+  AUTHOR,
+}
+
+const person = {
   name: 'yota',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
 
 // person.role.push('admin');
@@ -22,4 +35,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('管理者ユーザ');
 }
