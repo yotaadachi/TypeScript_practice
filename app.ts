303eafd486984fa94b4bfd4092e8fe641fc68da4
yotaadachi <yota961219@gmail.com>
2021-11-20 21:01:40 +0900

    Tuple型の学習

--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'yota',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
