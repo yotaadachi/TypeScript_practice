dbb342a7ceda33c4690b85c1ab595f5c92194936
yotaadachi <yota961219@gmail.com>
2021-11-21 08:39:01 +0900

    void型の学習

--- app.ts
+++ app.ts
@@ -1,34 +1,9 @@
-type Combinable = number | string;
-type conversionDescriptor = 'as-number' | 'as-text';
-
-
-function combine(
-  input1: Combinable, 
-  input2: Combinable, 
-  resultConversion: conversionDescriptor,
-) {
-  let result;
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') || 
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+printResult(add(5,12));
