5ffcc89db4e28a6acd161be5b34d759b95ab9b80
yotaadachi <yota961219@gmail.com>
2021-11-21 10:38:31 +0900

    never型の学習

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-var combineValues;
-combineValues = add;
-//combineValues = printResult;
-//combineValues = 5;
-console.log(combineValues(8, 8));
-printResult(add(5, 12));
+var result = generateError('エラーが発生しました', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code};
+}
+
+const result = generateError('エラーが発生しました', 500);
+console.log(result);
