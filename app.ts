8a7f886564e2ed426a15767308198dedb27f2288
yotaadachi <yota961219@gmail.com>
2021-11-21 10:03:11 +0900

    unknow型の学習

--- app.ts
+++ app.ts
@@ -1,26 +1,9 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-//combineValues = printResult;
-//combineValues = 5;
-
-console.log(combineValues(8,8));
-
-printResult(add(5,12));
