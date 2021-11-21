91e5d1f9eb1e8ac494d94e650ae38fe59f97060e
yotaadachi <yota961219@gmail.com>
2021-11-21 09:29:09 +0900

    コールバック関数の学習

--- app.ts
+++ app.ts
@@ -6,6 +6,15 @@ function printResult(num: number): void {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
+
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
